ee8852d704652a1593c03a899009f6f8cef41586
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-10-07 23:05:04 +0200

    Added driverRead for DriverResource

--- src/main/java/es/upm/miw/apaw/epc2/gabriel/munumel/api/controllers/DriverController.java
+++ src/main/java/es/upm/miw/apaw/epc2/gabriel/munumel/api/controllers/DriverController.java
@@ -1,6 +1,9 @@
 package es.upm.miw.apaw.epc2.gabriel.munumel.api.controllers;
 
+import java.util.Optional;
+
 import es.upm.miw.apaw.epc2.gabriel.munumel.api.daos.DaoFactory;
+import es.upm.miw.apaw.epc2.gabriel.munumel.api.dtos.DriverDto;
 import es.upm.miw.apaw.epc2.gabriel.munumel.api.entities.Driver;
 
 public class DriverController {
@@ -12,5 +15,8 @@ public class DriverController {
     public void createDriver(String driverReference, Long driverPhone) {
     		DaoFactory.getFactory().getDriverDao().create(new Driver(driverReference, driverPhone));
     }
-
+    
+    public Optional <DriverDto> readDriver(int driverId) {
+    		throw new java.lang.UnsupportedOperationException("Not implemented yet");
+     }
 }
--- src/main/java/es/upm/miw/apaw/epc2/gabriel/munumel/api/resources/DriverResource.java
+++ src/main/java/es/upm/miw/apaw/epc2/gabriel/munumel/api/resources/DriverResource.java
@@ -1,7 +1,11 @@
 package es.upm.miw.apaw.epc2.gabriel.munumel.api.resources;
 
+import java.util.Optional;
+
 import es.upm.miw.apaw.epc2.gabriel.munumel.api.controllers.DriverController;
+import es.upm.miw.apaw.epc2.gabriel.munumel.api.dtos.DriverDto;
 import es.upm.miw.apaw.epc2.gabriel.munumel.api.resources.exceptions.DriverFieldInvalidException;
+import es.upm.miw.apaw.epc2.gabriel.munumel.api.resources.exceptions.DriverIdNotFoundException;
 
 public class DriverResource {
 
@@ -9,8 +13,9 @@ public class DriverResource {
 
     public static final String ID = "/{id}";
 
-    public String readDriver(int driverId) {
-    		throw new java.lang.UnsupportedOperationException("Not implemented yet");
+    public DriverDto readDriver(int driverId) throws DriverIdNotFoundException {
+    		Optional<DriverDto> optional = new DriverController().readDriver(driverId);
+    		return optional.orElseThrow(() -> new DriverIdNotFoundException(Integer.toString(driverId)));
     }
     
     public void createDriver(String driverReference) throws DriverFieldInvalidException {
